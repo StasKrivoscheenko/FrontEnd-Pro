abd6dc47e6690665c08b07568176955035a6f904
stanislaw <staskrvoi.i@gmail.com>
2022-08-23 12:45:43 +0300

    finished

--- task 1/js.js
+++ task 1/js.js
@@ -1,5 +1,5 @@
 const a = prompt("Write first number");
 const b = prompt("Write second number");
 const result = ((+a)+(+b))
-alert(+a+'+'+b+'='+result)
+alert('Your answer is '+a+'+'+b+'='+result)
 
