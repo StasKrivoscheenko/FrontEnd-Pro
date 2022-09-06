ba546e37b8e51de97b7841bac4798fa0234c48a4
stanislaaw <staskrvoi.i@gmail.com>
2022-09-06 23:24:29 +0300

    finished

---
+++ task 3/js3.js
@@ -0,0 +1,38 @@
+const number = printNum();
+const evenSum = getEvenSum(number);
+const oddSum = getOddSum(number);
+console.log('Even sum: '+evenSum)
+console.log('Odd sum: '+oddSum)
+function printNum(){
+    let num;
+    do{
+        num = prompt('Print a number ');
+    }
+    while (isNumInvalid(num));
+    return Number(num);
+}
+function isNumInvalid(val){
+    return val === null || val.trim() === '' || isNaN(val) || val <= 0;
+}
+function getEvenSum(val){
+    let i = 0;
+    let result = 0;
+    while (i <= val){
+        if(i % 2 === 0){
+            result += 1;
+        }
+        i++
+    }
+    return evenSum
+}
+function getOddSum(val){
+    let i = 0;
+    let result = 0;
+    while (i <= val){
+        if(i % 2 === 1){
+            result += 1;
+        }
+        i++
+    }
+    return oddSum
+}
