87d85e694fbaa9ed82eb1b94a242bd0d562c15df
stanislaw <staskrvoi.i@gmail.com>
2022-11-07 16:07:10 +0200

    fa

---
+++ task 15/Accodion.js
@@ -0,0 +1,68 @@
+const DEFAULT_CONFIG = { pagination: false, collapseOthers: true };
+
+class Accordion {
+    static CLASSES = {
+        ITEM: 'accordion-item',
+        TITLE: 'accordion-title',
+        ACTIVE: 'accordion-active',
+    };
+
+    static SELECTORS = {
+        ITEM: '.accordion-item',
+        ACTIVE: '.accordion-active',
+    };
+
+    #el = null;
+    #config = null;
+    #currentIndex = null;
+
+    constructor(el, config = {}) {
+        this.#el = el;
+
+        this.#config = {
+            ...DEFAULT_CONFIG,
+            ...config,
+        };
+
+        this.#updateLayout();
+
+        this.#bindEventListeners();
+    }
+
+    #updateLayout() {
+        this.#el
+            .querySelectorAll(Accordion.SELECTORS.ITEM)
+            .forEach((item, index) => {
+                item.dataset.index = index;
+            });
+    }
+
+    #bindEventListeners() {
+        this.#el.addEventListener('click', (e) => {
+            if (e.target.classList.contains(Accordion.CLASSES.TITLE)) {
+                const item = e.target.closest(Accordion.SELECTORS.ITEM);
+                this.show(item.dataset.index);
+            }
+        });
+    }
+
+    show(index) {
+        const isVisible = this.#currentIndex === index;
+        if (isVisible) {
+            this.#el.children[index].classList.remove(Accordion.CLASSES.ACTIVE);
+        } else {
+            if (this.#config.collapseOthers) {
+                this.#hideAll();
+            }
+            this.#el.children[index].classList.add(Accordion.CLASSES.ACTIVE);
+        }
+
+        this.#currentIndex = index;
+    }
+
+    #hideAll() {
+        const els = this.#el.querySelectorAll(Accordion.SELECTORS.ACTIVE);
+
+        els.forEach((item) => item.classList.remove(Accordion.CLASSES.ACTIVE));
+    }
+}
