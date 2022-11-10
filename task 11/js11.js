71261605793eccfa7e43a46474874ce89edbac3d
stanislaw <staskrvoi.i@gmail.com>
2022-11-10 16:11:55 +0200

    da

--- task 11/js11.js
+++ task 11/js11.js
@@ -11,7 +11,7 @@ const newTodoForm = document.querySelector('#newTodoForm');
 
 let todoList = [
     { id: 1, title: 'Task 1', isDone: false },
-    { id: 2, title: 'Task 2', isDone: true },
+    { id: 2, title: 'Task 2', isDone: false },
     { id: 3, title: 'Task 3', isDone: false },
 ];
 
